78d62da2a7e22f2b4aba7b31a0edb506ef14dfc9
ravi nandan <monubibhu@gmail.com>
2024-02-27 16:20:06 +0530

    pqr

--- Studybud/settings.py
+++ Studybud/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ['DJANOG_SECRET_KEY']
 DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app", '.now.sh']
-# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -138,22 +137,22 @@ WSGI_APPLICATION = 'Studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studybud',
-        'USER':'default',
-        'PASSWORD':'36STvBNHblas',
-        'HOST':'ep-royal-salad-a1rdf3ki-pooler.ap-southeast-1.aws.neon.tech',
-        'PORT':'5432',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'studybud',
+#         'USER':'default',
+#         'PASSWORD':'36STvBNHblas',
+#         'HOST':'ep-royal-salad-a1rdf3ki-pooler.ap-southeast-1.aws.neon.tech',
+#         'PORT':'5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
