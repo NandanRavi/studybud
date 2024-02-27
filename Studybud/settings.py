ea4aa2271875c00e2af69ba4e4641209019e157a
ravi nandan <monubibhu@gmail.com>
2024-02-27 16:13:19 +0530

    xyz

--- Studybud/settings.py
+++ Studybud/settings.py
@@ -138,22 +138,22 @@ WSGI_APPLICATION = 'Studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'studybud',
-#         'USER':'postgres',
-#         'PASSWORD':'Zaraki9661@',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'studybud',
+        'USER':'default',
+        'PASSWORD':'36STvBNHblas',
+        'HOST':'ep-royal-salad-a1rdf3ki-pooler.ap-southeast-1.aws.neon.tech',
+        'PORT':'5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
