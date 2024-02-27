12bd53766da38db157bdf045d9365b36a113fb47
ravi nandan <monubibhu@gmail.com>
2024-02-27 21:18:55 +0530

    new one

--- Studybud/settings.py
+++ Studybud/settings.py
@@ -32,8 +32,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [".vercel.app", '.now.sh']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app", '.now.sh']
 
 
 # Application definition
